558375670776fdec30a078f2dbe1381a096aa099
Kai <kaijbooth@gmail.com>
2024-10-22 11:05:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import numpy as np
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Route for the home page
 @app.route("/")
 def home():
-    return render_template("index.html") 
+    return render_template("index.html")  # Renders the main page
 
+# Route for the graphs page
+@app.route("/graphs")
+def graphs():
+    return render_template("graphs.html")  # Renders the graphs page
 
 if __name__ == "__main__":
     app.run(debug=True)
